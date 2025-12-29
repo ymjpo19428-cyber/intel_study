48fac905adee13d6c96acbd19cbb11189b09debf
ymjpo19428-cyber <ymjpo19428@gmail.com>
2025-12-29 09:46:53 +0900

    Add files via upload

---
+++ C언어/예제 11-07.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num, grade;
+
+	printf("학번 입력 : ");
+	scanf("%d", &num);
+
+	getchar();
+	printf("학점 입력 : ");
+	grade = getchar();
+
+	printf("학번 : %d, 학점 : %c", num, grade);
+
+	return 0;
+}
