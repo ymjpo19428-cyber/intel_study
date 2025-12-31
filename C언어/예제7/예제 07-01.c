582b8e8c49e9e4c70ef147c08709977174a5e215
ymjpo19428-cyber <ymjpo19428@gmail.com>
2025-12-31 15:20:01 +0900

    Add files via upload

--- C언어/예제7/예제 07-01.c
+++ C언어/예제7/예제 07-01.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int sum(int x, int y);		//sum함수 선언 
+
+int main(void)				//main 함수 시작
+{
+	int a = 10, b = 20;		//두 정수를 더한 결과(result)를 저장할 변수
+	int result;
+
+	result = sum(a, b);		//sum 함수 호출
+
+	printf("result : %d\n", result);
+
+	return 0;
+}							//main 함수의 끝
+
+int sum(int x, int y)		//sum 함수 정의 시작
+{
+	int temp;				//두 정수의 합을 잠시 저장할 변수
+
+	temp = x + y;			//두 정수의 합을 temp에 보관
+
+	return temp;			//sum 함수의 끝
+}
