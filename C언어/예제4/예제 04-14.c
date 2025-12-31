b0180e7f128f4f01504cf7c89e79238f933e1583
ymjpo19428-cyber <ymjpo19428@gmail.com>
2025-12-31 10:52:56 +0900

    Add files via upload

---
+++ C언어/예제4/예제 04-14.c
@@ -0,0 +1,21 @@
+#include <stdio.h>	
+
+int main(void)
+{
+	int a = 10, b = 5;
+	int res;
+
+	res = a / b * 2;		//우선 순위가 같으므로 왼쪽 부터 차례로 연산
+	printf("res = %d\n", res);
+
+	res = ++a * 3;			//a의 값을 1 증가시키고 3을 곱함
+	printf("res = %d\n", res);
+		
+	res = a > b && a != 5;	//a>b의 결과와 a != 5의 결과를 && 연산
+	printf("res = %d\n", res);
+
+	res = a % 3 == 0;		//a%3의 값이 0과 같은지 확인
+	printf("res = %d\n", res);
+
+	return 0;
+}
