0323918943e858d1381f5a4cc2c28fcf2ecdfaa7
ymjpo19428-cyber <ymjpo19428@gmail.com>
2025-12-31 12:51:22 +0900

    Update 계산기 프로그램.c

--- C언어/예제5/계산기 프로그램.c
+++ C언어/예제5/계산기 프로그램.c
@@ -6,7 +6,7 @@ int main(void)
 	int num1, num2, result;
 	char sig;
 
-	printf("»çÄ¢¿¬»ê ÀÔ·Â(Á¤¼ö) : ");
+	printf("ì‚¬ì¹™ì—°ì‚° ìž…ë ¥(ì •ìˆ˜) : ");
 	scanf("%d%c%d", &num1, &sig, &num2);
 
 	if (sig == '+')
