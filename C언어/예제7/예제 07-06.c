d16bc5110355febc1a9f10dd79f6fa1e99d8e1d6
ymjpo19428-cyber <ymjpo19428@gmail.com>
2025-12-31 16:27:39 +0900

    Add files via upload

---
+++ C언어/예제7/예제 07-06.c
@@ -0,0 +1,21 @@
+#include <stdio.h>	
+
+void fruit(int count);
+
+int main(void)
+{
+	fruit(1);				//처음 호출하므로 1을 인수로 줌
+
+	return 0;
+}
+
+void fruit(int count)		//호출 횟수를 매개 변수에 저장
+{
+	printf("apple\n");
+
+	if (count == 3)			//호출회수가 3이면 
+	{
+		return;				//반환하고 끝냄
+	}
+	fruit(count + 1);		//재호출할때 호출 횟수를 1증가
+}
