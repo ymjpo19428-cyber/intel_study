e5e09a84181081d4887bd632643694216267cac1
ymjpo19428-cyber <ymjpo19428@gmail.com>
2025-12-31 11:36:48 +0900

    Add files via upload

---
+++ C언어/예제4/체중관리 프로그램.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>	
+
+int main(void)
+{
+	double weight, height;
+	double bmi;
+
+	printf("몸무게(kg)와 키(cm) 입력 : ");
+	scanf("%lf%lf", &weight, &height);
+
+	height = height / 100.0;
+
+	bmi = weight / (height * height);
+
+	((bmi >= 20.0) && (bmi < 25.0))
+		? printf("표준입니다.\n")
+		: printf("체중관리가 필요합니다\n");
+
+	return 0;
+
+
+}
